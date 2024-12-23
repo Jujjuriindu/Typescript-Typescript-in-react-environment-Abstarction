00a90c590e0202991a70669085a838a55c0aac61
Jujjuriindu <140819241+Jujjuriindu@users.noreply.github.com>
2024-12-23 16:20:22 +0530

    Add files via upload

---
+++ typescript/type.js
@@ -0,0 +1,14 @@
+let englishMarks = 90;
+let teluguMarks = 80;
+let hindiMarks = 75;
+let mathsMarks = 90;
+let scienceMarks =68;
+let socialMarks = 95
+
+englishMarks = "Hai"
+teluguMarks = true;
+
+let calculateMarks=(eng,tel,hin,mat,sci,soc)=>{
+    console.log(eng,tel,hin,mat,sci,soc);
+}
+calculateMarks(90,80,75,90,68,95);
---
+++ typescript/type1.ts
@@ -0,0 +1,19 @@
+let engMarks:number = 90;
+let telMarks:number = 80;
+let hinMarks:number = 75;
+let matMarks:number = 68;
+let sciMarks:number = 95;
+let socMarks:number = 78;
+
+engMarks = 90;
+telMarks = 80;
+hinMarks = 75;
+matMarks = 68;
+sciMarks = 95;
+socMarks = 78;
+
+let calculateResultMarks=(eng:number,tel:number,hin:number,mat:number,sci:number,soc:number)=>{
+    console.log(eng,tel,hin,mat,sci,soc);
+}
+
+calculateResultMarks(90,80,75,68,95,78);
